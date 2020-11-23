6aa683d7e14cb83f1784766255072c29081972e7
jieGit <2857705371@qq.com>
2020-11-23 23:04:53 +0800

    1-02

---
+++ index.js
@@ -0,0 +1,45 @@
+// console.log(global);
+
+const { argv } = require("process");
+
+
+// const obj = {
+//     setTimeout: function () {},
+//     console: {}
+// };
+// obj.global = obj;
+
+
+
+// const timer = setTimeout(() => {}, 1000);
+// console.log(timer);
+
+
+// const timer = setImmediate(() => {}, 0);
+// console.log("abc");
+
+
+// console.log(__dirname);
+
+
+// console.log("当前命令行", process.cwd());
+
+
+
+// setTimeout(() => {
+//     console.log("abc");
+// }, 1000);
+// process.exit();
+
+
+
+// console.log(process.argv);
+
+
+// console.log(process.platform);
+
+
+// process.kill(10428);
+
+console.log(process.env.HOMEDRIVE)
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// console.log(global.__dirname);
+// console.log(__filename);
+
+// const buffer = Buffer.from("abcdefg", "utf-8");
+// console.log(buffer);
+
+console.log("当前命令行", process.cwd());
