fb867301523c7231926ad3557644a6271e185f9d
jieGit <2857705371@qq.com>
2020-11-24 14:45:27 +0800

    1-07-1

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
 const fs = require("fs");
 const path = require("path");
+const filename = path.resolve(__dirname, "./abc.txt");
+const rs = fs.createReadStream(filename, {
+    encoding: "utf-8",
+    highWaterMark: 1,
+    autoClose: true//读完后会自动关闭
+});
 
-class File {
-    constructor (filename, name, ext, isFile, size, createTime, updateTime) {
-        this.filename = filename;
-        this.name = name;
-        this.ext = ext;
-        this.isFile = isFile;
-        this.size = size;
-        this.createTime = createTime;
-        this.updateTime = updateTime
-    }
-    async getContent (isBuffer = false) {
-        if (this.isFile) {
-            if (isBuffer) {
-                return await fs.promises.readFile(this.filename);
-            } else {
-                return await fs.promises.readFile(this.filename, "utf-8");
-            }
-        }
-        return null;
-    }
+rs.on("open", ()=> {
+    console.log("文件被打开了");
+});
 
-    async getChildren () {
-        if (this.isFile) {//文件不可能有子文件
-            return [];
-        }
-        let children = await fs.promises.readdir(this.filename);
-        children = children.map(name => {
-            const result = path.resolve(this.filename, name);
-            return File.getFile(result);
-        });
-        return Promise.all(children);
-    }
+rs.on("error", () => {
+    console.log("出错了!");
+});
 
-    static async getFile (filename) {
-        const stat = await fs.promises.stat(filename);
-        const name = path.basename(filename);
-        const ext = path.extname(filename);
-        const isFile = stat.isFile();
-        const size = stat.size;
-        const createTime = new Date(stat.birthtime);
-        const updateTime = new Date(stat.mtime);
-        return new File(filename, name, ext, isFile, size, createTime, updateTime);
-    }
-}
+rs.on("close", () => {
+    console.log("文件关闭时触发");
+});
 
-async function readDir (dirname) {
-const file = await File.getFile(dirname);
-return await file.getChildren();
-}
 
-async function test () {
-    const dirname = path.resolve(__dirname, "./myfiles");
-    const result = await readDir(dirname);
-    const datas = await result[0].getChildren();
-    console.log(datas);
-}
-test();
+
+rs.on("data", (chunk) => {
+    console.log("读到了一部分数据:", chunk);
+    rs.pause();//暂停
+});
+
+rs.on("pause", () => {
+    console.log("暂停了");
+    setTimeout(() => {
+        rs.resume();
+    },1000);
+});
+
+rs.on("resume", () => {
+    console.log("恢复了");
+});
+
+
+
+rs.on("end", () => {
+    console.log("全部数据读取完毕");
+});
