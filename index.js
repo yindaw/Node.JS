1597d9cf828838db3ca54ca26ea0793130cf5729
jieGit <2857705371@qq.com>
2020-11-24 21:39:32 +0800

    1-08

--- index.js
+++ index.js
@@ -1,52 +1,28 @@
+const net = require("net");
+const server = net.createServer();
 const fs = require("fs");
-const path = require("path");
+const path = require("path"); 
+server.listen(9527);//服务器监听9527端口
 
-async function method1 () {
-    const from = path.resolve(__dirname, "./temp/abc.txt");
-    const to = path.resolve(__dirname, "./temp/abc2.txt");
-    console.time("方式1");
-    const content = await fs.promises.readFile(from);
-    await fs.promises.writeFile(to, content);
-    console.timeEnd("方式1");
-    console.log("复制完成");
-}
-method1();
+server.on("listening", () => {
+    console.log("server listen 9527");
+});
 
+server.on("connection", (socket) => {
+    console.log("有客户端连接到服务器");
+    socket.on("data", async (chunk) => {
+        // console.log(chunk.toString("utf-8"));
+        const filename = path.resolve(__dirname, "./hsq.jpg");
+        const bodyBuffer = await fs.promises.readFile(filename);
+        const headBuffer = Buffer.from(`HTTP/1.1 200 OK
+Content-Type: image/jpg
 
-
-方式2
-async function method2 () {
-    const from = path.resolve(__dirname, "./temp/abc.txt");
-    const to = path.resolve(__dirname, "./temp/abc2.txt");
-    console.time("方式2");
-   
-    const rs = fs.createReadStream(from);
-    const ws = fs.createWriteStream(to);
-    // rs.on("data", chunk => {
-    //     //读到一部分数据
-    //     const flag = ws.write(chunk);
-    //     if (!flag) {
-    //         //表示下一次写入，会造成背压
-    //         rs.pause();
-    //     }
-    // });
-    // ws.on("drain", () => {
-    //     //可以继续写了
-    //     rs.resume();
-    // })
-    // rs.on("close", () => {
-    //     //写完了
-    //     ws.end();//完毕写入流
-    //     console.timeEnd("方式2");
-    //     console.log("复制完成");
-    // })
-
-
-
-
-    rs.pipe(ws);
-    rs.on("close", () => {
-        console.timeEnd("方式2");
+`, "utf-8");
+        const result = Buffer.concat([headBuffer, bodyBuffer]);
+        socket.write(result);
+        socket.end();
     });
-}
-method2();
+    socket.on("end", () => {
+        console.log("连接关闭了");
+    });
+});
