81051a679b15baa9b525b1b19a74fdd71884aae7
jieGit <2857705371@qq.com>
2020-11-28 10:02:23 +0800

    3-04

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-require("./models/sync");
+const adminServ = require("./services/adminService");
+// adminServ.addAdmin({
+//     loginId: "abcsasasaas",
+//     loginPwd: "asdfgsg"
+// });
+
+
+adminServ.deleteAdmin(4).then(r => {
+    console.log(r);
+});
+
+// adminServ.updateAdmin(4, {
+//     loginId: "aaaa",
+// }).then(r => {
+//     console.log(r);
+// }); 
