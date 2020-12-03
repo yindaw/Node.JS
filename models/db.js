fd390f4da7bdfbd7719a20551d8ce7230366e62a
jieGit <2857705371@qq.com>
2020-12-03 09:48:41 +0800

    3-10

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("myschooldb", "root", "123123", {
     host: "localhost",
-    dialect: "mysql"
-    // logging: null
+    dialect: "mysql",
+    logging: null
 });
 module.exports = sequelize;
